a668802ff3c70b3e8d8d8c5e9481f4b8e83bc318
Davy <davykavanagh@gmail.com>
2015-11-09 21:18:55 -0500

    add docstrings; delete image of item on item delete.

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ import httplib2
 import json
 import requests
 from functools import wraps
-import filecmp
+import datetime
 from flask import Flask, render_template, redirect, request, url_for
 from flask import make_response, flash, abort, jsonify
 from flask import session as login_session
@@ -37,7 +37,7 @@ APPLICATION_NAME = "UDACITY Catalog App"
 
 
 STATIC_DIR = 'static/'
-ALLOWED_IMG_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_IMG_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app.config.update({
     'STATIC_DIR': 'static',
@@ -46,6 +46,12 @@ app.config.update({
 
 
 def generate_csrf_token():
+    """
+    generate the cross site request forgery token (random 32
+    character string), and add it to the login_session.
+
+    :return: csrf token
+    """
     if '_csrf_token' not in login_session:
         login_session['_csrf_token'] = ''.join(
             random.choice(
@@ -55,15 +61,30 @@ def generate_csrf_token():
     return login_session['_csrf_token']
 
 
-def check_for_csrf():
+def check_for_csrf(supplied_token):
+    """
+    Check for the csrf token in the login session and the one
+    submitted by the form. If the token is None or not the
+    same as the one in the login_session return a 403
+    (unauthorized) error. If it is the same, return None
+
+    :return: None or abort(403)
+    """
     token = login_session.pop('_csrf_token', None)
-    if not token or token != request.form['_csrf_token']:
+    if not token or token != supplied_token:
         abort(403)
     else:
         return None
 
 
 def login_required(func_to_dec):
+    """
+    Makes a decorator for protecting pages from anonymous non-logged
+    in users.
+
+    :param function (view) that requires user to be logged in:
+    :return: decorated function.
+    """
     @wraps(func_to_dec)
     def decorated_view(*args, **kwargs):
         if 'username' not in login_session:
@@ -75,6 +96,12 @@ def login_required(func_to_dec):
 
 @app.context_processor
 def inject_categories():
+    """
+    Context processor to ensure categories sidebar is on every page.
+
+    :return: dictionary to the categories and their counts to inserted
+    into the response context.
+    """
     # May limit to first whatever number
     categories = session.query(Category).all()
 
@@ -85,15 +112,17 @@ def inject_categories():
         CatalogItem
     ).group_by(Category.id).order_by('num DESC')
 
-    for c in cat_counts:
-        print c.Category.title
-        print c.num
-
     return dict(categories=categories, cat_counts=cat_counts)
 
 
 @app.route('/login')
 def show_login():
+    """
+    Show the login page, and generate a state token to prevent Man
+    in the middle attack.
+
+    :return: rendered template of login page
+    """
     state = ''.join(
         random.choice(
             string.ascii_uppercase + string.digits
@@ -104,6 +133,13 @@ def show_login():
 
 
 def allowed_file(filename):
+    """
+    Check if the extension of the supplied file is an allowed
+    type as defined in the app.config dict.
+
+    :param filename: name of file to check extension of.
+    :return: true if filename ext is allowed.
+    """
     allowed = False
     if '.' in filename:
         extension = filename.rsplit('.', 1)[1]
@@ -114,26 +150,34 @@ def allowed_file(filename):
 
 
 def store_image_to_media(image_object):
-    if image_object and allowed_file(image_object.filename):
-        filename = secure_filename(image_object.filename)
-        print filename
-        # save path to image without 'static/' prepended to path
+    """
+     This function takes the image object submitted by either the
+     create or edit item forms, and it ensures the file name is secure
+     and prepends it with the current timestamp (down to the
+     microsecond).
+     It then saves the image in the STATIC_DIR/ITEM_IMAGES folder
+     as defined by app.config, and returns the appropriate path
+      - ITEM_IMAGES/filename - for saving to the database.
 
+    :param image_object:
+    :return: path to image for saving in the DB, or None if the image
+    object is none OR the allowed_file check is false.
+    """
+    if image_object or allowed_file(image_object.filename):
+        filename = secure_filename(image_object.filename)
+
+        # prepend timestamp to filename
+        now = datetime.datetime.now()
+        filename = now.strftime(
+            '%Y%m%d%H%M%S%f_{fname}'
+        ).format(fname=filename)
+
+        # save path to image without 'static/' prepended to path
         db_path = os.path.join(app.config['ITEM_IMAGES'], filename)
         # but actually save in static
         save_path = os.path.join(app.config['STATIC_DIR'], db_path)
-        print db_path
-        print save_path
 
-        if os.path.exists(save_path):
-            tmp_path = save_path + '_tmp'
-            image_object.save(tmp_path)
-            if not filecmp.cmp(save_path, tmp_path):
-                os.remove(save_path)
-                os.rename(tmp_path, save_path)
-
-        else:
-            image_object.save(save_path)
+        image_object.save(save_path)
 
         return db_path
 
@@ -141,8 +185,28 @@ def store_image_to_media(image_object):
         return None
 
 
+def delete_item_image(item_obj):
+    """
+    delete the image associated with a catalog item.
+
+    :param item_obj: item object
+    :return: True if item deleted, False if OSerror (likely because
+    file not found)
+    """
+    try:
+        os.remove(os.path.join('static', item_obj.image_path))
+        return True
+    except OSError:
+        return False
+
+
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """
+    Connect to Google + and sign the user in.
+
+    :return: various response objects
+    """
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -225,7 +289,13 @@ def gconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect(next_url='/'):
-        # Only disconnect a connected user.
+    """
+    disconnect from Google+ and sign the user out.
+
+    :param next_url: the next page to be forwared onto
+    :return: various redirects, with flash messages.
+    """
+    # Only disconnect a connected user.
     access_token = login_session.get('credentials')
     if access_token is None:
         flash("No User connected!", 'alert-danger')
@@ -255,6 +325,11 @@ def gdisconnect(next_url='/'):
 
 @app.route('/')
 def index():
+    """
+    Show home page
+
+    :return: rendered tempate of home/index page
+    """
     items = session.query(CatalogItem).all()
 
     return render_template('index.html', items=items)
@@ -262,6 +337,12 @@ def index():
 
 @app.route('/item/<int:item_id>/')
 def view_item(item_id):
+    """
+    View single item page
+
+    :param item_id: id of the item to be viewed
+    :return: rendered template of the view item page.
+    """
     item = session.query(CatalogItem).get(item_id)
     return render_template('view_item.html', item=item)
 
@@ -269,7 +350,14 @@ def view_item(item_id):
 @app.route('/item/add/', methods=['GET', 'POST'])
 @login_required
 def add_item():
+    """
+    Show page with add item form.
+    If the form was filled out, process the form and add new item
+    to database.
 
+    :return: Redirect the user to the new item page on
+    a successful additon, else return to the add_item page.
+    """
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
@@ -298,6 +386,16 @@ def add_item():
 @app.route('/item/edit/<int:item_id>/', methods=['GET', 'POST'])
 @login_required
 def edit_item(item_id):
+    """
+    Show page with add item form.
+    If the form was filled out, process the form update the item
+    in the database. Make sure old item image is deleted and new
+    image is saved.
+
+    :param item_id: id of item to be edited
+    :return: Redirect the user to the updated item page on
+    a successful edit else return to edit_item page.
+    """
     item = session.query(CatalogItem).get(item_id)
 
     if request.method == 'POST':
@@ -315,6 +413,8 @@ def edit_item(item_id):
         image = request.files['image']
         image_path = store_image_to_media(image)
         if image_path is not None:
+            # delete the old image, and then update the new image path
+            delete_item_image(item)
             item.image_path = image_path
 
         session.add(item)
@@ -330,12 +430,21 @@ def edit_item(item_id):
 @app.route('/item/delete/<int:item_id>/', methods=['GET', 'POST'])
 @login_required
 def delete_item(item_id):
+    """
+    Show the delete_item page. If the form is filled out check for
+    csrf token, and make sure the item image is also deleted.
+
+    :param item_id: id of item to be deleted.
+    :return: Redirect the user to the category page of the deleted
+    item, or the delete_item page, or the view_item page on a cancelled
+    delete.
+    """
     item = session.query(CatalogItem).get(item_id)
     category_id = item.category.id
     if request.method == 'POST':
 
-        check_for_csrf()
-
+        check_for_csrf(request.form['_csrf_token'])
+        delete_item_image(item)
         session.delete(item)
         session.commit()
 
@@ -350,6 +459,12 @@ def delete_item(item_id):
 
 @app.route('/category/<int:category_id>/')
 def view_category(category_id):
+    """
+    View Category page and list of all items in that category.
+
+    :param category_id: id of requested category.
+    :return: rendered template of category page
+    """
     category = session.query(Category).get(category_id)
     items = session.query(CatalogItem).filter_by(category_id=category_id)
     return render_template(
@@ -362,6 +477,13 @@ def view_category(category_id):
 @app.route('/category/add/', methods=['GET', 'POST'])
 @login_required
 def add_category():
+    """
+    Show add category page with form. If form has been filled out,
+    processs the form, and add new category to the database.
+
+    :return: rendered template of the add category page or the new
+    category on successful addition.
+    """
     if request.method == 'POST':
         title = request.form['category-title']
         new_category = Category(title=title)
@@ -377,12 +499,24 @@ def add_category():
 # JSON API METHODS HERE
 @app.route('/api/json/item/<int:item_id>/', methods=['GET'])
 def get_item_json(item_id):
+    """
+    Return json reprsentation of item.
+
+    :param item_id: id of item to return
+    :return: jsonified serialized item.
+    """
     item = session.query(CatalogItem).get(item_id)
     return jsonify(item.serialize())
 
 
 @app.route('/api/json/category/<int:category_id>/')
 def get_category_json(category_id):
+    """
+    Return json reprsentation of a category and its items.
+
+    :param category_id: id of category to return
+    :return: jsonified serialized category and items
+    """
     category = session.query(Category).get(category_id)
     items = session.query(CatalogItem).filter_by(category_id=category_id)
     return jsonify(
@@ -393,12 +527,22 @@ def get_category_json(category_id):
 
 @app.route('/api/json/item/')
 def get_all_items_json():
+    """
+    Return json reprsentation of all items
+
+    :return: jsonified serialized items - all of them
+    """
     items = session.query(CatalogItem).all()
     return jsonify(Items=[i.serialize() for i in items])
 
 
 @app.route('/api/json/category/')
 def get_all_categories_json():
+    """
+    Return json reprsentation of all categories
+
+    :return: jsonified serialized categories
+    """
     cats = session.query(Category).all()
     return jsonify(Categories=[i.serialize() for i in cats])
 
